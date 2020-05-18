f44740ab4ee2f4c6992f82a6c8006719fe5536d6
amir.barshavit <amir.barshavit@naturalint.com>
2020-05-18 16:54:26 +0300

    refactoring button css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import Button from "./Components/Button/Button";
 
 // import images from "./mock.json";
 import { useImages } from "./Hooks/fetchImages";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faSync } from "@fortawesome/free-solid-svg-icons";
-
 const getRandomImages = (images = [], numberOfImages = 5) => {
   const shuffled = images.sort(() => 0.5 - Math.random());
   return shuffled.slice(0, numberOfImages);
 };
 
 function App() {
-  const [images, setImages] = useImages(
-    '"https://api.jonathanczyzyk.com/api/v1/images/small"'
-  );
+  const [images] = useImages();
   const [displayImages, setDisplayImages] = useState();
   const onRefreshButtonClick = () => {
     const chosenImages = getRandomImages(images, 5);
@@ -29,11 +24,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="gallery">
-        <Gallery images={displayImages} />
-        <div className="button-warper">
-          <Button onClick={onRefreshButtonClick}>Refresh</Button>
-        </div>
+      <Gallery images={displayImages} />
+      <div className="button-warper">
+        <Button onClick={onRefreshButtonClick}>Refresh</Button>
       </div>
     </div>
   );
--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,10 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Button.css";
 function Button(props) {
   const { onClick, className, disabled } = props;
+
   const onButtonClick = () => {
     onClick && onClick();
   };
+
   return (
     <button
       className={`button ${className}`}
@@ -15,5 +18,17 @@ function Button(props) {
     </button>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  disabled: false,
+  className: "",
+};
 
 export default Button;
