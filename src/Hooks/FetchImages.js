3e0cfad3c9aeba6c48311892058d89284b4df5a0
amir.barshavit <amir.barshavit@naturalint.com>
2020-05-20 11:55:22 +0300

    file naming  bug

---
+++ src/Hooks/FetchImages.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function useApiImages() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let res = await fetch(
+          "https://api.jonathanczyzyk.com/api/v1/images/small",
+          {
+            headers: {
+              "x-api-key": "api-key-d35ba445-9f45-430e-881c-944df0cf0f8e",
+            },
+          }
+        );
+        res = await res.json();
+        setData(res);
+      } catch (err) {
+        throw err;
+      }
+    }
+    fetchData();
+  }, []);
+
+  return [data, setData];
+}
